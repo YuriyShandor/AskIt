ffed9f3c08a7c574a179716157cd91197bd7df1d
Yuriy Shandor <yuriyshandor@gmail.com>
2018-01-11 14:17:24 +0200

    Add Section Partners

--- js/script.js
+++ js/script.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  var partnersSlider = $('.partners-slider');
+  var maxItems = Math.round(partnersSlider.parent('div').width() / 250);
+  if(partnersSlider.children('div').length < maxItems) {
+    maxItems = partnersSlider.children('div').length;
+  }
+
+  partnersSlider.slick({
+    infinite: true,
+    variableWidth: true,
+    slidesToShow: maxItems,
+    slidesToScroll: 1,
+    nextArrow: $(".next_arrow"),
+    prevArrow: $(".prev_arrow"),
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
 });
